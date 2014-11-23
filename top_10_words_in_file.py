49f15223c61df93ca81855111ba5723c523078ea
Syed Ali <syed_a_ali@yahoo.com>
2014-11-23 09:46:32 -0800

    added top_10_words_in_file.py

---
+++ top_10_words_in_file.py
@@ -0,0 +1,12 @@
+# given an input file called 'infile.txt', find the top 10 most
+# commonly occuring words and print them out
+
+author__ = 'syedaali'
+
+import re
+import collections
+
+words = re.findall(r'\w+', open('infile.txt').read().lower())
+
+print collections.Counter(words).most_common(10)
+
