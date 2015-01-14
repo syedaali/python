49695700560b67b4cb1d929eaf9059ba5b3cd6d3
Syed Ali <syed_a_ali@yahoo.com>
2015-01-14 09:54:26 -0800

    added code to functions that were place holders

--- mean_mode_median.py
+++ mean_mode_median.py
@@ -1,11 +1,35 @@
 __author__ = 'syedaali'
 
+import numpy as np
+import collections
+
 
 def f_mean(l):
-    return sum(l)/len(l)
+    return float(sum(l)/len(l))
+
+
+def f_median(l):
+    n = np.array(l)
+    return np.mean(n)
 
 def f_mode(l):
 
+    # if list is unique, then there is no mode
+    if len(l) == len(set(l)):
+        return 'None'
 
-def f_mean(l):
+    #create dictionary based on numbers of values of each
+    c = collections.Counter(l)
 
+    #return key of dict that has the max value
+    single = max(c.iterkeys(),key=lambda k: c[k])
+
+    return single
+
+alist = [1,2,2,3,3,4,5]
+
+print f_mean(alist)
+
+print f_median(alist)
+
+print f_mode(alist)
